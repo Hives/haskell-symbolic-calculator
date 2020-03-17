22b0a75e2fe4354959da6e58407507c0f4c24817
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-03-17 02:07:20 +0000

    Create tokenizer

--- symbolicCalculator.hs
+++ symbolicCalculator.hs
@@ -1,8 +1,40 @@
-data Token
+import Data.Char
+
+data Token = TokOp Operator
+           | TokIdent String
+           | TokNum Int
+           deriving (Show, Eq)
+
+showContent :: Token -> String
+showContent (TokOp op)     = opToStr op
+showContent (TokIdent str) = str
+showContent (TokNum i)     = show i
+
 data Expression
 
+data Operator = Plus | Minus | Times | Div
+   deriving (Show, Eq)
+
+operator :: Char -> Operator
+operator c | c == '+' = Plus
+           | c == '-' = Minus
+           | c == '*' = Times
+           | c == '/' = Div
+
+opToStr :: Operator -> String
+opToStr Plus  = "+"
+opToStr Minus = "-"
+opToStr Times = "*"
+opToStr Div   = "/"
+
 tokenize :: String -> [Token]
-tokenize = undefined
+tokenize [] = []
+tokenize (c:cs)
+   | elem c "+-*/" = TokOp (operator c)    : tokenize cs
+   | isDigit c     = TokNum (digitToInt c) : tokenize cs
+   | isAlpha c     = TokIdent [c]          : tokenize cs
+   | isSpace c     =                         tokenize cs
+   | otherwise     = error $ "Cannot tokenize " ++ [c]
 
 parse :: [Token] -> Expression
 parse = undefined
@@ -10,8 +42,7 @@ parse = undefined
 evaluate :: Expression -> Double
 evaluate = undefined
 
-main :: IO ()
-main = do
-   line <- getLine
-   putStrLn line
-   main
+token :: Token
+token = TokIdent "x"
+
+main = print $ tokenize " 1 + 4 / x "
